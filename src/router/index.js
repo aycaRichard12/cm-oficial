711604e8ede782b1aac098ff09479b8cdbc9988f
aycaRichard12 <aycarichard7@gmail.com>
2025-12-18 13:47:31 -0400

    DR

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,27 @@
-import { defineRouter } from '#q-app/wrappers'
+// import { defineRouter } from '#q-app/wrappers'
 
-import { createRouter, createMemoryHistory } from 'vue-router'
-import routes from './routes'
+// import { createRouter, createMemoryHistory } from 'vue-router'
+// import routes from './routes'
 
-export default defineRouter(function () {
-  const createHistory = createMemoryHistory
+// export default defineRouter(function () {
+//   const createHistory = createMemoryHistory
 
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
-    history: createHistory(),
-  })
+//   const Router = createRouter({
+//     scrollBehavior: () => ({ left: 0, top: 0 }),
+//     routes,
+//     history: createHistory(),
+//   })
 
-  return Router
-})
+//   return Router
+// })
+
+import { useRouter } from 'vue-router'
+
+setup () {
+  const router = useRouter()
+
+  console.log(router.getRoutes())
+
+  return {}
+}
+
