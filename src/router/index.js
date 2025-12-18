fefcd37615689828fe736cddb5ee6a88bf11fc0f
aycaRichard12 <aycarichard7@gmail.com>
2025-12-18 15:28:44 -0400

    Chore(rutas): Configurar correctamente el historial del router segun(SSR/SPA y modo (history / hash)

--- src/router/index.js
+++ src/router/index.js
@@ -15,13 +15,27 @@
 //   return Router
 // })
 
-import { useRouter } from 'vue-router'
+import { defineRouter } from '#q-app/wrappers'
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+  createWebHashHistory,
+} from 'vue-router'
+import routes from './routes'
 
-setup () {
-  const router = useRouter()
+export default defineRouter(function () {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : process.env.VUE_ROUTER_MODE === 'history'
+      ? createWebHistory
+      : createWebHashHistory
 
-  console.log(router.getRoutes())
+  const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  })
 
-  return {}
-}
-
+  return Router
+})
