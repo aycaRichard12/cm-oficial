7385e62b9f48d0d4bd7819be9a77868fee2ec00d
DEX <toladavi369@gmail.com>
2026-01-07 16:20:44 -0400

    se agrego el filtro a la en el archivo TableCompra.vue y se organizo el codigo en 3 nuevos arhcivos, CompraActions.vue donde estan los botones de nueva compra y ver pdf, CompraDialogs.vue aqui esta los componetes para ver el pdf y registrar creditos  y CompraFilters.vue qui esta el buscador y el select de almacenes

---
+++ src/components/compra/CompraFilters.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row q-col-gutter-x-md flex justify-between q-mb-md">
+    <div class="col-12 col-md-4">
+      <label for="almacen">Seleccione un Almacén</label>
+      <q-select
+        :model-value="filtroAlmacen"
+        @update:model-value="$emit('update:filtroAlmacen', $event)"
+        :options="almacenes"
+        id="almacen"
+        clearable
+        dense
+        outlined
+        
+      />
+    </div>
+    <div class="col-12 col-md-2">
+      <label for="buscar">Buscar...</label>
+      <q-input
+        dense
+        debounce="300"
+        :model-value="busqueda"
+        @update:model-value="$emit('update:busqueda', $event)"
+        id="buscar"
+        outlined
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  almacenes: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+  filtroAlmacen: {
+    type: Object,
+    default: null,
+  },
+  busqueda: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['update:filtroAlmacen', 'update:busqueda'])
+</script>
