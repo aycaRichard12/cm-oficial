fe3035b7c675b0b145faaf09e1fe59821c091bbd
DEX <toladavi369@gmail.com>
2026-01-07 16:20:44 -0400

    se agrego el filtro a la en el archivo TableCompra.vue y se organizo el codigo en 3 nuevos arhcivos, CompraActions.vue donde estan los botones de nueva compra y ver pdf, CompraDialogs.vue aqui esta los componetes para ver el pdf y registrar creditos  y CompraFilters.vue qui esta el buscador y el select de almacenes

---
+++ src/components/compra/CompraActions.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row items-center justify-between q-mb-sm">
+    <div class="col-4 q-mt-lg flex flex-col gap-4">
+      <!-- Botón de nueva compra -->
+      <q-btn
+        color="primary"
+        class="btn-res"
+        id="btnNuevaCompra"
+        @click="$emit('add')"
+        icon="add"
+        label="Nueva Compra"
+        no-caps
+      />
+      
+    </div>
+
+    <div class="col-2 q-mt-lg flex justify-end">
+      <q-btn color="info" @click="$emit('imprimirReporte')" class="btn-res" outline>
+        <q-icon name="picture_as_pdf" class="icono" />
+        <span class="texto">Vista Previa PDF</span>
+      </q-btn>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineEmits(['add', 'imprimirReporte'])
+</script>
+
+
