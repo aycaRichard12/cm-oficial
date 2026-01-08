3e596e160882e0dedeeefb140370997dc9723255
DEX <toladavi369@gmail.com>
2026-01-08 18:15:03 -0400

    nueva guia agregada, se el TOUR de la pagina PRODUCTOS O SERVICIOS en la seccion de Parametros Obsolescencia

---
+++ src/tours/configuracion/Tour_parametrosdeobsolescencia.js
@@ -0,0 +1,47 @@
+export const Tour_parametrosdeobsolescencia = [
+    {
+        element: '#agregarParametro',
+        popover: {
+            title: 'Agregar Parametro',
+            description: 'Agregar un nuevo parametro de obsolescencia',
+            side: 'bottom',
+            align: 'start',
+        },
+    },
+    {
+        element: '#buscarParametro',
+        popover: {
+            title: 'Buscar Parametro',
+            description: 'Buscar un parametro de obsolescencia, por los campos de la tabla',
+            side: 'bottom',
+            align: 'start',
+        },
+    },
+    {
+        element: '#tablaParametro',
+        popover: {
+            title: 'Tabla de Parametros',
+            description: 'Tabla de Parametros de Obsolescencia, aqui se muestran todos los parametros de obsolescencia',
+            side: 'bottom',
+            align: 'start',
+        },
+    },
+    {
+        element: '#editarParametro',
+        popover: {
+            title: 'Editar Parametro',
+            description: 'Editar un parametro de obsolescencia, seleccionando el parametro para cambiar algo que no le gusta',
+            side: 'bottom',
+            align: 'start',
+        },
+    },
+    {
+        element: '#eliminarParametro',
+        popover: {
+            title: 'Eliminar Parametro',
+            description: 'Eliminar un parametro de obsolescencia, seleccionando el parametro para eliminarlo',
+            side: 'bottom',
+            align: 'start',
+        },
+    },
+]
