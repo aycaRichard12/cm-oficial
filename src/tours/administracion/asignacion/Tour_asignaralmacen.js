5b71a7fa3a786505b504ac46761efaa2c6c6b18b
DEX <toladavi369@gmail.com>
2026-01-12 16:33:01 -0400

    Agregado identificadores a los elementos de la tabla de asignación y creado un tour para guiar a los usuarios en la gestión de asignaciones.

---
+++ src/tours/administracion/asignacion/Tour_asignaralmacen.js
@@ -0,0 +1,83 @@
+export const Tour_asignaralmacen = [
+  {
+    element: "#asignaralmacen",
+    popover: {
+      title: "Asignar Almacenes",
+      description: "Clic para entrar a la sección de asignación de almacenes",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#asignarpuntodeventa",
+    popover: {
+      title: "Asignar Punto de Venta",
+      description: "Clic para asignar un punto de venta",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#asignarproductos",
+    popover: {
+      title: "Asignar Productos",
+      description: "Clic para asignar productos",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#categoriasdeprecio",
+    popover: {
+      title: "Categorías de Precio",
+      description: "Clic para asignar productos",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#nuevaAsignacion",
+    popover: {
+      title: "Nueva Asignación",
+      description: "Clic para que a un usuario se le asignen almacenes",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#buscarAsignacion",
+    popover: {
+      title: "Buscar Asignación",
+      description: "Escribe para buscar a un usuario responsable de almacenes",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#tablaAsignacion",
+    popover: {
+      title: "Tabla de Asignación",
+      description: "Tabla que muestra los datos de los usuarios responsables de almacenes",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#eliminarAsignacion",
+    popover: {
+      title: "Eliminar Asignación",
+      description: "Clic para eliminar la asignación de todos los almacenes que tiene un usuario responsable",
+      side: "bottom",
+      align: "start",
+    },
+  },
+  {
+    element: "#asignarAlmacenes",
+    popover: {
+      title: "Asignar Almacenes",
+      description: "Clic para asignar más almacenes a un usuario responsable",
+      side: "bottom",
+      align: "start",
+    },
+  },
+];
