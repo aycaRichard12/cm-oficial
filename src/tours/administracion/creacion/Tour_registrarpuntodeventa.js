26e4e572b6dd48259e71766c85fd24771d130f03
DEX <toladavi369@gmail.com>
2026-01-12 15:33:26 -0400

    Agregado tour para la gestión de puntos de venta, incluyendo descripciones para los elementos de agregar, buscar, editar y eliminar puntos de venta.

---
+++ src/tours/administracion/creacion/Tour_registrarpuntodeventa.js
@@ -0,0 +1,77 @@
+
+
+
+export const Tour_registrarpuntodeventa = [
+    {
+        element: '#registrarpuntodeventa',
+        popover: {
+          title: 'Registrar Punto de Venta',
+          description: 'Haz clic aquí para abrir el formulario y registrar un nuevo punto de venta.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#agregarPuntoVenta',
+        popover: {
+          title: 'Agregar Punto de Venta',
+          description: 'Haz clic aquí para agregar un nuevo punto de venta.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#tipoAlmacen',
+        popover: {
+          title: 'Tipo de Almacén',
+          description: 'Selecciona el tipo de almacén para el punto de venta.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#buscarPuntoVenta',
+        popover: {
+          title: 'Buscar Punto de Venta',
+          description: 'Busca un punto de venta por nombre.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#tablaPuntosVenta',
+        popover: {
+          title: 'Tabla de Puntos de Venta',
+          description: 'Verifica la lista de puntos de venta.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#asignarPuntoVenta',
+        popover: {
+          title: 'Asignar Punto de Venta',
+          description: 'Asigna un punto de venta a un usuario.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#editarPuntoVenta',
+        popover: {
+          title: 'Editar Punto de Venta',
+          description: 'Edita la información de un punto de venta.',
+          side: 'bottom',
+          align: 'start',
+        },
+    },
+    {
+        element: '#eliminarPuntoVenta',
+        popover: {
+          title: 'Eliminar Punto de Venta',
+          description: 'Elimina un punto de venta.',
+          side: 'bottom',
+          align: 'start',
+        },
+    }
+]
