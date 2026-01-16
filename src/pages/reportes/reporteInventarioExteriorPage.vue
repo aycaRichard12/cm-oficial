1a71fe8fdd12d2bd45be581a3da19cde32eea090
DEX <toladavi369@gmail.com>
2026-01-16 16:38:50 -0400

    Eliminada la clase de desbordamiento en la sección de tabla del reporte de inventario exterior.

--- src/pages/reportes/reporteInventarioExteriorPage.vue
+++ src/pages/reportes/reporteInventarioExteriorPage.vue
@@ -9,7 +9,7 @@
       </div>
     </div>
 
-    <div class="row q-col-gutter-md overflow-hidden">
+    <div class="row q-col-gutter-md">
       <div class="col-12">
         <q-card>
           <q-card-section>
