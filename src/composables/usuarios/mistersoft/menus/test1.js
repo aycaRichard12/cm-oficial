67d25226facbff20845c5634b024e7337f155f9b
aycaRichard12 <aycarichard7@gmail.com>
2026-01-20 15:14:13 -0400

    COnfiguracion Usuarios

---
+++ src/composables/usuarios/mistersoft/menus/test1.js
@@ -0,0 +1,31 @@
+export const menutest1 = [
+  {
+    modulo: '3',
+    menu: [
+      {
+        usuario: '7f39f8317fbdb1988ef4c628eba02591',
+        titulo: 'Compras',
+        codigo: 'compras',
+        submenu: [
+          {
+            titulo: 'Registrar Compra o Producción',
+            codigo: 'registrarcompra-7f39f8317fbdb1988ef4c628eba02591',
+            permiso: '1111',
+          },
+        ],
+      },
+      {
+        usuario: '7f39f8317fbdb1988ef4c628eba02591',
+        titulo: 'Ventas',
+        codigo: 'ventas',
+        submenu: [
+          {
+            titulo: 'Generar Venta',
+            codigo: 'registrarventa-7f39f8317fbdb1988ef4c628eba02591',
+            permiso: '1111',
+          },
+        ],
+      },
+    ],
+  },
+]
