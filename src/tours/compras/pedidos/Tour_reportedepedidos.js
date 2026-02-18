1a128d464702afaa0894fbfb92b1b0708803f5cf
DEX <toladavi369@gmail.com>
2026-02-18 16:50:30 -0400

    configuracion de guias para compras/pedidos

---
+++ src/tours/compras/pedidos/Tour_reportedepedidos.js
@@ -0,0 +1,75 @@
+export const Tour_reportedepedidos = [
+
+    {
+        element: '#reportedepedidos',
+        popover: {
+            title: 'Reporte de Pedidos',
+            description: 'Reporte de Pedidos que se generara.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#pedidos',
+        popover: {
+            title: 'Fecha Inicial y Fecha Final del Reporte',
+            description: 'Debe seleccionar una Fecha Inicial y Fecha Final del Reporte.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#generarreporte',
+        popover: {
+            title: 'Generar Reporte',
+            description: 'Generar Reporte del periodo seleccionado.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#vistaprevia',
+        popover: {
+            title: 'Vista Previa',
+            description: 'Vista Previa del Reporte.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#almacen',
+        popover: {
+            title: 'Almacén',
+            description: 'Almacén del Reporte.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#tablaPedidos',
+        popover: {
+            title: 'Tabla',
+            description: 'Tabla de los Pedidos.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#verpedido',
+        popover: {
+            title: 'Ver Pedido',
+            description: 'Ver Pedido seleccionado.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#enviarpdf',
+        popover: {
+            title: 'Enviar PDF',
+            description: 'Enviar PDF al WhatsApp.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    }
+]
