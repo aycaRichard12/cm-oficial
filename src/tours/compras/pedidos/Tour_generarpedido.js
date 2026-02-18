c6c187574481697c1ee1b3477af58b26e26ac46d
DEX <toladavi369@gmail.com>
2026-02-18 16:50:30 -0400

    configuracion de guias para compras/pedidos

---
+++ src/tours/compras/pedidos/Tour_generarpedido.js
@@ -0,0 +1,134 @@
+export const Tour_generarpedido = [
+    {
+        element: '#generarpedido',
+        popover: {
+            title: 'Generar Pedido',
+            description: 'Generar Pedido de tipo Movimiento o Compra.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnAgregar',
+        popover: {
+            title: 'Agregar',
+            description: 'Agregar un nuevo pedido de tipo Movimiento o Compra.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnImprimir',
+        popover: {
+            title: 'Imprimir',
+            description: 'Imprimir la lista de pedidos.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },{
+        element: '#almacenPedidos',
+        popover: {
+            title: 'Almacén',
+            description: 'Almacén del pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },{
+        element: '#tipoPedidos',
+        popover: {
+            title: 'Tipo',
+            description: 'Tipo de pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },{
+        element: '#buscarPedidos',
+        popover: {
+            title: 'Buscar',
+            description: 'Buscar    pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#tablePedidos',
+        popover: {
+            title: 'Tabla',
+            description: 'Tabla de pedidos.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnVerPDF',
+        popover: {
+            title: 'Ver PDF',
+            description: 'Ver PDF del pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnVerImagen',
+        popover: {
+            title: 'Ver Imagen',
+            description: 'Ver Imagen del pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnVerDetalle',
+        popover: {
+            title: 'Ver Detalle',
+            description: 'Ver Detalle del pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnEditar',
+        popover: {
+            title: 'Editar',
+            description: 'Editar el pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnEliminar',
+        popover: {
+            title: 'Eliminar',
+            description: 'Eliminar el pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#autorizar',
+        popover: {
+            title: 'Autorizar',
+            description: 'Autorizar el pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#notificaciones',
+        popover: {
+            title: 'Notificaciones',
+            description: 'Notificaciones del pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnSubirBaucher',
+        popover: {
+            title: 'Subir Baucher',
+            description: 'Subir Baucher del pedido.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    }
+]
