7f686e8ca093907f0130ee099b7b7cee06883317
DEX <toladavi369@gmail.com>
2026-02-18 16:21:50 -0400

    configuracion de guias en campanas

---
+++ src/tours/administracion/crearcampanas/Tour_crearcampanas.js
@@ -0,0 +1,87 @@
+
+export const Tour_crearcampanas = [
+
+    {
+        element: '#crearcampanas',
+        popover: {
+            title: 'Campaña',
+            description: 'Administra y crea campañas.',
+            side: 'bottom',
+            align: 'start',
+        },
+    },
+    {
+        element: '#nuevaCampana',
+        popover: {
+            title: 'Nueva Campaña',
+            description: 'Crea una nueva campaña.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#filtroAlmacen',
+        popover: {
+            title: 'Filtro Almacén',
+            description: 'Filtro de Almacén.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#filtroBusqueda',
+        popover: {
+            title: 'Filtro Búsqueda',
+            description: 'Filtro de Búsqueda.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#estadoCampana',
+        popover: {
+            title: 'Estado de la Campaña',
+            description: 'Estado de la Campaña.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnCategoria',
+        popover: {
+            title: 'Gestionar Categorías',
+            description: 'Gestionar Categorías.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnProductos',
+        popover: {
+            title: 'Gestionar Productos',
+            description: 'Gestionar Productos.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnEditar',
+        popover: {
+            title: 'Editar',
+            description: 'Editar una campaña.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    {
+        element: '#btnEliminar',
+        popover: {
+            title: 'Eliminar',
+            description: 'Eliminar una campaña.',
+            side: 'bottom',
+            align: 'start', 
+        }
+    },
+    
+
+]
