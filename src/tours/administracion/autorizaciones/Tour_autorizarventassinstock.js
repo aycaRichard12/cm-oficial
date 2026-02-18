34d39e7b2d9bac6c1f0917ac798b7d0aa7a0ac03
DEX <toladavi369@gmail.com>
2026-02-18 16:09:49 -0400

    configuracion de guias tour par Autorizaciones

---
+++ src/tours/administracion/autorizaciones/Tour_autorizarventassinstock.js
@@ -0,0 +1,88 @@
+
+export const Tour_autorizarventassinstock = [
+    
+        {element: '#autorizarventassinstock',
+        popover: {
+            title: 'Autorizar Ventas sin Stock',
+            description: 'Autorizar ventas sin stock para despachar el producto.',
+            side: 'bottom',
+            align: 'start',
+        }
+    },   
+        {
+            element: '#solicitudes',
+            popover: {
+                title: 'Solicitudes',
+                description: 'Solicitudes de permiso.',
+                side: 'bottom',
+                align: 'start',
+            },
+        },
+        {
+            element: '#activos',
+            popover: {
+                title: 'Permisos Activos',
+                description: 'Permisos activos.',
+                side: 'bottom',
+                align: 'start',
+            },
+        },
+        {
+            element: '#historial',
+            popover: {
+                title: 'Historial',
+                description: 'Historial de permisos.',
+                side: 'bottom',
+                align: 'start',
+            },
+        },
+        {
+            element: '#vencidos',
+            popover: {
+                title: 'Vencidos',
+                description: 'Permisos vencidos.',
+                side: 'bottom',
+                align: 'start',
+            },
+        },
+        {
+            element: '#gestionar',
+            popover: {
+                title: 'Gestionar Solicitud',
+                description: 'Gestionar solicitud de permiso.',
+                side: 'bottom',
+                align: 'start',
+            },
+        },
+
+            {
+            element: '#activos',
+            popover: {
+                title: 'Permisos Activos',
+                description: 'Click para continuar con el tour.',
+                side: 'bottom',
+                align: 'start',
+            },
+        },
+        // {
+        //        element: '#tablaActivos' ,
+        //        popover: {
+        //         title: 'Tabla de Permisos Activos',
+        //         description: 'Tabla de permisos activos.',
+        //         side: 'bottom',
+        //         align: 'start',
+        //     },
+        // },
+        // {
+        //     element : '#historial',
+        //     popover: {
+        //         title: 'Historial',
+        //         description: 'Click para continuar con el tour para ver el historial de permisos.',
+        //         side: 'bottom',
+        //         align: 'start',
+        //     },
+        // },
+   
+
+    
+]
