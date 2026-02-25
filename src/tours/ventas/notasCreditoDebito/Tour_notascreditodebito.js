93ffc25174ec1bec592572fd090d5809faa79c46
DEX <toladavi369@gmail.com>
2026-02-25 16:17:16 -0400

    configuracoin de guias para notas credito/debito e inventario externo

---
+++ src/tours/ventas/notasCreditoDebito/Tour_notascreditodebito.js
@@ -0,0 +1,107 @@
+export const Tour_notascreditodebito = [
+    {
+        element: '#fechaini',
+        popover: {
+            title: 'Fecha Inicial',
+            description: 'Selecciona la fecha de inicio para buscar las notas de crédito/débito.',
+        }
+    },
+    {
+        element: '#fechafin',
+        popover: {
+            title: 'Fecha Final',
+            description: 'Selecciona la fecha límite de búsqueda.',
+        }
+    },
+    {
+        element: '#btnGenerarReporte',
+        popover: {
+            title: 'Generar reporte',
+            description: 'Presiona este botón para filtrar los datos con las fechas seleccionadas.',
+        }
+    },
+    {
+        element: '#vistaPrevia',
+        popover: {
+            title: 'Vista previa',
+            description: 'Genera un reporte en archivo PDF de las notas filtradas.',
+        }
+    },
+    {
+        element: '#exportarExcel',
+        popover: {
+            title: 'Exportar Excel',
+            description: 'Exporta los datos de las notas de crédito/débito a un documento de Excel.',
+        }
+    },
+    {
+        element: '#almacenCredito',
+        popover: {
+            title: 'Filtrar por Almacén',
+            description: 'Selecciona un almacén específico para listar sus notas.',
+        }
+    },
+    {
+        element: '#clienteCredito',
+        popover: {
+            title: 'Razón social',
+            description: 'Busca y selecciona un cliente por su razón social para ver solo sus notas correspondientes.',
+        }
+    },
+    {
+        element: '#dialogSucursal',
+        popover: {
+            title: 'Sucursal del Cliente',
+            description: 'Selecciona la sucursal asignada a este cliente.',
+        }
+    },
+    {
+        element: '#dialogCanal',
+        popover: {
+            title: 'Canal de Venta',
+            description: 'Filtra las notas dependiendo del canal de venta empleado.',
+        }
+    },
+    {
+        element: '#dialogTipoPago',
+        popover: {
+            title: 'Tipo de Pago',
+            description: 'Elige si la nota provino de una orden a crédito o al contado.',
+        }
+    },
+    {
+        element: '#tableCredito',
+        popover: {
+            title: 'Desglose de Notas',
+            description: 'En esta tabla podrás visualizar, revisar detalles, reenviar o imprimir cada nota de manera individualizada.',
+        }
+    },
+    {
+        element: '#crearMensaje',
+        popover: {
+            title: 'Crear Mensaje',
+            description: 'Crea un mensaje para los clientes.',
+        }
+    },
+    {
+        element: '#ir_a_factura',
+        popover: {
+            title: 'Ir a Factura',
+            description: 'Ir a la factura correspondiente.',
+        }
+    },
+    {
+        element: '#ir_a_impuestos',
+        popover: {
+            title: 'Ir a Impuestos',
+            description: 'Ir a la factura correspondiente.',
+        }
+    },
+    {
+        element: '#ir_a_NotaCreditoDebito',
+        popover: {
+            title: 'Ir a Nota de Credito/Debito',
+            description: 'Ir a la Nota de Credito/Debito correspondiente.',
+        }
+    }
+]
