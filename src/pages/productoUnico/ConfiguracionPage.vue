37abf80c54372f8090abc2a8cefb230fd81df8b1
aycaRichard12 <aycarichard7@gmail.com>
2026-04-04 09:46:21 -0400

    se añadio cambio CODIGO UNICO PRODUCTO

---
+++ src/pages/productoUnico/ConfiguracionPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <configuracioPU :md5="md5" />
+  </q-page>
+</template>
+<script setup>
+import configuracioPU from 'src/components/productoUnico/configuracioPU.vue'
+import { idempresa_md5 } from 'src/composables/FuncionesGenerales'
+
+const md5 = idempresa_md5()
+</script>
